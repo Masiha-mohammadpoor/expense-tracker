a39d6075adca71c313524c88af3421aa31de257c
Masiha mhmdpour <masihmooohmmadpoor@gmail.com>
2022-08-18 16:45:34 +0430

    update amount added

--- src/components/TransactionMain.jsx
+++ src/components/TransactionMain.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState  , useEffect} from "react";
 import OverView from "./OverView";
 import TransactionList from "./TransactionList";
 
@@ -9,9 +9,21 @@ const TransactionMain = () => {
     const [income, setIncome] = useState(0);
 
 
+    useEffect(() => {
+        let exp = 0,
+        inc = 0;
+        transactions.map(t => {
+            t.type === "expense" ? exp += parseFloat(t.amount) : inc += parseFloat(t.amount);
+        })
+        setExpense(exp);
+        setIncome(inc);
+    }, [transactions])
+
     const addTransaction = (value) => {
         setTransactions([...transactions , value])
     }
+
+
     return (
         <main className="main">
             <OverView income={income} expense={expense} addTransaction={addTransaction}/>
